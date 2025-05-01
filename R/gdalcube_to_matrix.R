02b098b849c6130c6a2b79add30feba1934e63dd
swhammon <43630679+swhammon@users.noreply.github.com>
2025-05-01 14:30:12 -0600

    update

---
+++ R/gdalcube_to_matrix.R
@@ -0,0 +1,42 @@
+#' Download data from spat4cast-data bucket and turn it into a cube raster 
+#'
+#' @param cube A data cube proxy object (gdalcubes) to extract values from
+#' @param d dimension value object for rastercube 'cube'
+#' @param threshold: numeric, upper bounds for LAI. values above threshold are discarded
+#' @param dir character, directory to write temporary GeoTiff files to
+#' @return matrix of LAI values indexed by time which may be used to fit arima-grid models
+#' @examples 
+#' @export
+#' 
+
+
+
+## function to extract nimble matrix
+gdalcube_to_matrix <- function(cube, d, threshold = 12, dir = '../GeoTiffs/'){
+  ## Outputs:
+  ## nimble_mat: matrix of LAI values indexed by time to be
+  ## passed to nimble to fit the baseline spatio-temporal
+  ## random walk model.
+  
+  ## initialize nimnble_mat using information from d object
+  mat <- matrix(NA, nrow = length(d$t), 
+                       ncol = length(d$x)*length(d$y))
+  ## loop through time indices of d
+  for (i in 1:length(d$t)){
+    ## slice cube at time i and write GeoTiff file
+    cube %>% gdalcubes::select_time(d$t[i]) %>% write_tif(dir = dir, prefix = 'tmp')
+    ## recreate filename
+    filename <- paste0(dir, 'tmp', d$t[i], '.tif')
+    ## read in file as raster
+    tmp_rast <- rast(filename, vsi = TRUE)
+    ## extract values from raster
+    tmp_values <- as.vector(terra::values(tmp_rast))
+    ## subset values using threshold
+    tmp_values[tmp_values > threshold] <- NA
+    ## write i-th row of nimble matrix using raster values
+    mat[i,] <- tmp_values
+    ## remove temporary GeoTiff file
+    if (paste0('tmp', d$t[i], '.tif') %in% list.files(dir)) file.remove(filename)
+  }
+  return(mat)
+}
---
+++ R/spat4cast_get_data.R
@@ -0,0 +1,59 @@
+#' Download data from spat4cast-data bucket and turn it into a cube raster 
+#'
+#' @param start_date start date as character format yyyy-mm-dd
+#' @param end_date end date as character format yyyy-mm-dd
+#' @param box numeric vector in the format of (xmin, ymin, xmax, ymax)
+#' @param asset_name mame of asset 
+#' @param srs target spatial reference system as a string; can be a proj4 definition, WKT, or in the form "EPSG:XXXX"
+#' @param dx size of pixels in x-direction (longitude / easting)
+#' @param dy size of pixels in y-direction (latitude / northing)
+#' @param dt size of pixels in time-direction, expressed as ISO8601 period string (only 1 number and unit is allowed) such as "P16D"
+#' @param aggregation aggregation method as string, defining how to deal with pixels containing data from multiple images, can be "min", "max", "mean", "median", or "first"
+#' @param resampling resampling method used in gdalwarp when images are read, can be "near", "bilinear", "bicubic" or others as supported by gdalwarp (see https://gdal.org/programs/gdalwarp.html)
+#' @return A data cube proxy object
+#' @examples 
+#' ingest_planetary_data(start_date = "2022-01-01", end_date = "2023-07-01", box =  c("xmin" = -123, "ymin" = 39, "xmax" = -122, "ymax" = 40))
+#' @export
+#' 
+spat4cast_get_data <- function(start_date = "2002-01-01",
+                                  end_date = "2025-03-01",
+                                  box,
+                                  collection = "modis-15A2H-061",
+                                  asset_name = "Lai_500m",
+                                  srs = "EPSG:4326",
+                                  dx = 0.1, 
+                                  dy = 0.1, 
+                                  dt = "P30D",
+                                  aggregation = "mean",
+                                  resampling = "near"){
+  
+  # check box
+  assertthat::are_equal(length(box), 4)
+  
+  # set mc_alias
+  mc_alias_set("efi", "data.ecoforecast.org", "", "")
+  
+  # create temporary directory to hold files
+  dir.create("files")
+  
+  # Copy files from miniobucket
+  mc_cp("efi/spat4cast-data/variable=lai_recovery/site_id=august_complex/", "files", recursive = TRUE)
+  
+  # extract dates from copied files
+  d <-str_extract(dir_ls("files/"), "(?<=/).*(?=\\.)")
+  
+  # set dimensions of the cube
+  v <- gdalcubes::cube_view(srs = srs, #lat/lon
+                            extent = list(t0 = as.character(start_date), t1 = as.character(end_date),
+                                          left = box[1], right = box[3],
+                                          top = box[4], bottom = box[2]),
+                            dx = dx, dy = dy, dt= dt,
+                            aggregation = aggregation, resampling = resampling)
+  
+  # create image collection
+  cube <- gdalcubes::create_image_collection(dir_ls("files/"), date_time = d)
+  
+  # create proxy data cube
+  proxy_cube <- gdalcubes::raster_cube(cube, v)
+  return(proxy_cube)
+}
