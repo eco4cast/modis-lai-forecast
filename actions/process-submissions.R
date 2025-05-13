138196f0db0ea75d16b47481e46f971166489017
swhammon <43630679+swhammon@users.noreply.github.com>
2025-05-12 21:11:09 -0600

    Update process-submissions.R

--- actions/process-submissions.R
+++ actions/process-submissions.R
@@ -6,8 +6,11 @@ install_mc()
 mc_alias_set("efi", "data.ecoforecast.org",
              Sys.getenv("AWS_ACCESS_KEY_ID"), Sys.getenv("AWS_SECRET_ACCESS_KEY"))
 
+mc_alias_set("efi", "data.ecoforecast.org",
+             "spatial-team", "EXk4HARe6AXoZRR3uVwQ")
 
-for(fire in c(list.files("shp"))){
+  
+fire <- "august_complex"
   
   submitted <- mc_ls(paste0("efi/spat4cast-submissions/duration=P1M/variable=lai_recovery/site_id=",fire,"/"), recursive = TRUE)
   
@@ -16,11 +19,11 @@ for(fire in c(list.files("shp"))){
   mc_cp(paste0("efi/spat4cast-submissions/duration=P1M/variable=lai_recovery/site_id=",fire,"/"), "submissions",recursive =  TRUE)
   
   for (i in 1:length(submitted)){
-    forecast <- str_extract(submitted[i], "(?<=reference_date=.{10}/).*(?=\\.)")
+    forecast <- str_extract(submitted[i], "(?<=reference_date=.{10}/).*")
     
     dir.create("forecasts")
     temp_rast <- rast(paste0("submissions/", submitted[i]))
     writeRaster(temp_rast, paste0("forecasts/",forecast), filetype = "COG", overwrite = TRUE)
     mc_cp(paste0("forecasts/",forecast), paste0("efi/spat4cast-forecasts/duration=P1M/variable=lai_recovery/site_id=",fire,"/",submitted[i]), recursive = TRUE)
   }
-}
+
